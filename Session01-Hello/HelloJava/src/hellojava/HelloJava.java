f7ccaf964ae6818ad91978e14bfd3e5e3043f4f1
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-07 15:31:46 +0700

    Add Session01-Hello

---
+++ Session01-Hello/HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,16 @@
+package hellojava;
+
+/**
+ *
+ * @author gia.thinh
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        // ~ printf("Xin chao Java...\n"); 
+        //f11, bên này f6 dịch sau đó chạy app xem kết quả
+        System.out.println("5 + 5 = " + 5 + 5);
+        System.out.println("5 + 5 = " + (5 + 5));
+        
+    }
+}

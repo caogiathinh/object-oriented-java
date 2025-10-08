ebbaf61917643fcc46a544f406bcfc0e648fead8
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-08 12:16:20 +0700

    Upload Session03

---
+++ Session03-Class/DogManagement/src/data/Dog.java
@@ -0,0 +1,15 @@
+package data;
+
+/**
+ *
+ * @author gia.thinh
+ */
+//khuôn dùng để đút ra các object, đối tượng cần lưu trữ info, thứ
+//làm ra để bán
+//khuôn phải kín, có phễu để rót/hứng/đỏ vào cái data, nguyên liệu
+public class Dog {
+    //chứa các đặc điểm chi tiết mà cái Khuôn, object tương lai sẽ có
+    //Khuôn design ra sao, mai mốt object có thế
+}
+
+//code bên ngoài giống như cái khuôn bị lủng
---
+++ Session03-Class/DogManagement/src/dogmanagement/DogManagement.java
@@ -0,0 +1,12 @@
+package dogmanagement;
+
+/**
+ *
+ * @author gia.thinh
+ */
+public class DogManagement {
+    public static void main(String[] args) {
+        
+    }
+    
+}

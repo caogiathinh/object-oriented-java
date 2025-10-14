8d86308e8b6f41925d585eacc3c78423c6ab2e0f
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-14 23:07:40 +0700

    Update Session04-MyToysV3

--- Session04-Essentials/MyToysV3/src/thinh/main/Stage.java
+++ Session04-Essentials/MyToysV3/src/thinh/main/Stage.java
@@ -11,8 +11,18 @@ import thinh.util.MyToys;
  * @author gia.thinh
  */
 public class Stage {
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) { //psvm -> tab: viết tắt của hàm main 
         System.out.println("PI: " + MyToys.PI);
         
+//        int n = MyToys.getAInteger();
+//        
+//        System.out.println("n: " + n);
+        int choice;
+        choice = MyToys.getAInteger("Ipnut your choice (1..7): "); 
+        System.out.println("Your choice is: " + choice);
+        
     }
+    
+    
 }
--- Session04-Essentials/MyToysV3/src/thinh/util/MyToys.java
+++ Session04-Essentials/MyToysV3/src/thinh/util/MyToys.java
@@ -13,7 +13,28 @@ public class MyToys {
 
     public static final double PI = 3.1415;
     //final ~ const - HẰNG SỐ 
+    //đẩy Scanner ra ngoài xài chung cho tất cả các hàm
+    private static Scanner sc = new Scanner(System.in); //để tránh trùng lại, nhưng private dùng nội bộ bên trong
 
+    public static int getAInteger(String inputMsg) {
+        int n; //biến trung gian, hứng từ bàn phím gõ gì ?? 
+
+        //Scanner sc = new Scanner(System.in); //khai báo đối tượng sc
+
+        do {
+            try {
+                System.out.print(inputMsg);
+                n = Integer.parseInt(sc.nextLine());
+                //kiểm tra nhập ngoài biên
+                return n;
+
+            } catch (Exception e) {
+                System.out.println("Do ku know input an integer??");
+
+            }
+        } while (true);
+    }
+    
     //trong quá trình làm app, việc nhập số gặp lại nhiều lần. 
     //mỗi cú nhập, biến nhập , kèm thêm trôi lệnh, cà chớn try-catch - do while 
     //tách việc nhập số nguyên thành 1 hàm gói việc chửi bới, trôi lệnh, nhập lại một chỗ 
@@ -27,6 +48,7 @@ public class MyToys {
             try {
                 System.out.print("Input an integer: ");
                 n = Integer.parseInt(sc.nextLine());
+                //kiểm tra nhập ngoài biên
                 return n;
 
             } catch (Exception e) {
@@ -35,6 +57,8 @@ public class MyToys {
             }
         } while (true);
     }
+    
+    
     
     
 

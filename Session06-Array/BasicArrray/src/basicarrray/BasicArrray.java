07561d09ff678262dd21b8ce53ca8a01ba6305e1
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-16 09:23:27 +0700

    Update Session06-Array

---
+++ Session06-Array/BasicArrray/src/basicarrray/BasicArrray.java
@@ -0,0 +1,28 @@
+
+package basicarrray;
+
+/**
+ *
+ * @author gia.thinh
+ */
+public class BasicArrray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static void playWithPrimitiveArray() {
+        //double vol[365];  JAVA cấm khai báo kích thước mảng, số phần tử trong []
+            
+        //double v[365] = {0, 0, 5, 10, 15, 20};
+        double v[] = {0, 0, 5, 10, 15, 20, 25}; //mảng có 7 phần tử, có sẵn luôn value
+        //7 biến float nằm trên HEAP đấy
+        //có new ngầm đấy
+        System.out.println("The array has value of {printed by individual}");
+
+    }
+    
+}

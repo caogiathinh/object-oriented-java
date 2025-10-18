28bfc43635e3a0005e3e265546d33d85d8c4335b
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-18 23:45:06 +0700

    Update Session06-Array Object Sorting

--- Session06-Array/StudentManagement/src/main/Stage.java
+++ Session06-Array/StudentManagement/src/main/Stage.java
@@ -1,17 +1,49 @@
 
 package main;
 
+
+import java.util.Scanner;
+import data.Student;
+
 /**
  *
  * @author gia.thinh
  */
 public class Stage {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        inputStudentList();
     }
     
+    public static void inputStudentList() {
+        Scanner sc = new Scanner(System.in);
+        String id, name;
+        int yob, count;
+        double gpa;
+        
+        System.out.println("How mayny students do you want to input? ");
+        count = Integer.parseInt(sc.nextLine()); //MyToys.getAnInteger(???)
+        
+        Student[] arr = new Student[count];
+       
+        for (int i = 0; i < count; i++) {
+            System.out.print("Input id: ");
+            id = sc.nextLine(); 
+            
+            System.out.print("Input name: ");
+            name = sc.nextLine();
+            
+            System.out.print("Input yob: ");
+            yob = Integer.parseInt(sc.nextLine());
+            
+            System.out.print("Input gpa: ");
+            gpa = Double.parseDouble(sc.nextLine()); 
+            
+            arr[i] = new Student(id, name, yob, gpa);
+
+        }
+        System.out.println("Student list");
+        for (Student x : arr) {
+            x.showProfile();
+        }
+    }
 }

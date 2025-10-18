1409750deee43b11a1117e024c36b07fc7f70f33
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-18 23:09:11 +0700

    Update Session06-Array Object Sorting

--- Session06-Array/BasicArrayV2/src/basicarrayv2/Sorting.java
+++ Session06-Array/BasicArrayV2/src/basicarrayv2/Sorting.java
@@ -13,12 +13,59 @@ public class Sorting {
      
     public static void main(String[] args) {
         System.out.println("Ahihi");
+        //sortPrimitiveArray();
+        sortStudentList();
+    }
+    
+    public static void sortStudentList() {
+        Student arr[] = new Student[]{new Student("SE9", "CHIN", 19, 9), 
+                                      new Student("SE5", "NAM", 15, 5),
+                                      new Student("SE7", "BAY", 17, 7),
+                                      new Student("SE3", "BA", 19, 3),
+                                      new Student("SE8", "TAM", 18, 8)
+                                     };
+        System.out.println("The student list before sorting");
+        for (Student x : arr) {
+            x.showProfile();
+        }
         
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i].getGpa() > arr[j].getGpa()) {
+                    Student t = arr[i]; 
+                    arr[i] = arr[j]; 
+                    arr[j] = t; 
+                }
+            }
+        }
+        
+        System.out.println("The student list after sorting ascending by gpa");
+        for (Student x : arr) {
+            x.showProfile();
+        }
     }
     
     public static void sortPrimitiveArray() {
-        int arr[] = new int[100]; 
+        int arr[] = new int[]{1000, -1000, 1000, 5, 10, -15, -20}; 
+        System.out.println("Before sorting ascending");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
         
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) { //i trước j mày đứng trước tao mà lại lớn hơn tao đổi
+                    int t = arr[i]; 
+                    arr[i] = arr[j]; 
+                    arr[j] = t; 
+                }
+            }
+        }
+
+        System.out.println("\nAfter sorting ascending");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
             
 }

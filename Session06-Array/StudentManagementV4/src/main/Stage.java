5115b40a8f06d2ba14f01c02e5394164950ba4f7
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-20 16:48:59 +0700

    Update Session06-Array StudentManagementV4 - add menu

--- Session06-Array/StudentManagementV4/src/main/Stage.java
+++ Session06-Array/StudentManagementV4/src/main/Stage.java
@@ -1,6 +1,7 @@
 package main;
 
 import data.Shelf;
+import java.util.Scanner;
 
 /**
  *
@@ -12,6 +13,45 @@ public class Stage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //ráp menu cảm giác 1 cái app 
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        Shelf tuSE = new Shelf("PINK", "SE");
+        do {
+            printMenu();
+            System.out.print("Input your choice (1..6): "); //my Toys là ngon nhất
+            choice = Integer.parseInt(sc.nextLine());
+            switch (choice) {
+                case 1:
+                    tuSE.addAStudent();
+                    break;
+                case 2:
+                    tuSE.printStudentList();
+                    break;
+
+                case 6:
+                    System.out.println("Bye bye, see you next time");
+                    break;
+                default:
+                    System.out.println("Please choose 1..6 options");
+                    break;
+            }
+        } while (choice != 6); //còn đúng còn làm
+
+    }
+
+    public static void printMenu() {
+        System.out.println("Welcome to SIS");
+        System.out.println("Choose the following functions to play with");
+        System.out.println("1. Add a new student profile");
+        System.out.println("2. Print a student list");
+        System.out.println("3. Search a student by id");
+        System.out.println("4. Update a student profile");
+        System.out.println("5. Remove a student profile");
+        System.out.println("6. Quit");
+    }
+
+    public static void testShelf() {
         Shelf tuSE = new Shelf("PINK", "SE MAJOR");
         tuSE.addAStudent();
         tuSE.addAStudent();

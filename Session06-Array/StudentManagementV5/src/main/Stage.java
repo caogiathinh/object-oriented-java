08ddcd23c3cb30dbe452e20861df21bfb6547734
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-21 13:43:17 +0700

    Update Session06-Array StudentManagementV5

--- Session06-Array/StudentManagementV5/src/main/Stage.java
+++ Session06-Array/StudentManagementV5/src/main/Stage.java
@@ -12,11 +12,22 @@ public class Stage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Shelf tuSE = new Shelf("PINK", "SE"); 
-        
-        tuSE.addAStudent();
-        
-        tuSE.printStudentList();
+        int choice; 
+        do {            
+            
+        } while (true);
     }
+    
+    public static void printMenu() {
+        System.out.println("Welcome to SIS");
+        System.out.println("Choose the following function play with");
+        System.out.println("1. Add a new student profile");
+        System.out.println("2. Print the student list");
+        System.out.println("3. Search a student by id");
+        System.out.println("4. Update a student profile");
+        System.out.println("5. Remove a student profile");
+        
+    }
+           
    
 }

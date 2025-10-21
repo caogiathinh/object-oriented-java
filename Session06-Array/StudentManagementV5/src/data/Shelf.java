66001f791562ff606340e1fd0b7602cee1a29897
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-21 07:41:54 +0700

    Update Session06-Array StudentManagementV5

---
+++ Session06-Array/StudentManagementV5/src/data/Shelf.java
@@ -0,0 +1,24 @@
+
+package data;
+
+/**
+ *
+ * @author gia.thinh
+ */
+public class Shelf {
+    private String color; 
+    private String label;
+    
+    private Student[] ds = new Student[300];
+    
+    private int count = 0; 
+
+    public Shelf(String color, String label) {
+        this.color = color;
+        this.label = label;
+    }
+    
+    public void addAStudent() {{
+        
+    }
+}

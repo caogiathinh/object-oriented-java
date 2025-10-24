7ce8fba13a28d5d66cde3318f9f081761858c180
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-24 23:16:53 +0700

    Update Session06-Inheritance

--- Session06-Inheritance/CraftsmanV3/src/data/Square.java
+++ Session06-Inheritance/CraftsmanV3/src/data/Square.java
@@ -6,48 +6,41 @@ package data;
  * @author gia.thinh
  */
 
-//cách chế tạo cái khuôn mới từ khuôn cũ, kế thừa lại, đứng trên vai .. 
-//không phải là COPY/PASTE
-//phiên bản cũ là mình làm các Khuôn từ đầu, không kế thừa, sp chỏi nhau 
+//hình vuông là phiên bản đặc biệt của hcn
+//không mất đi bản sắc hcn là 4 góc vuông
+//hv là sự mở rộng, nâng cấp, đặc biệt hóa, phiên bản mới 
+//chỉnh/thay đổi nhỏ từ hcn 
+//cách chế tạo khuôn từ khuôn cũ 
+//chế tạo khuôn mới từ khuôn cũ, kế thừa, dùng lại
+//không phải là copy/paste
+//phiên bản cũ làm các Khuôn từ đầu, không kế thừa, sản phầm chỏi nhau. 
 //không cùng 1 nhà được 
 
-//1. mới A Extends cũ B
-//2.                   HCN Khuôn gốc để độ: Class Cha, parent, super, base
-//      HV              kế thừa từ HCN:     Class Con, child, sub, derived
-                                                       //biến hình, chế ra 
-                                                       //xuất phát từ
-//2. Đặc điểm của con là gì ?
-//              vì HV là phiên bản của HCN, nên nó sẽ có tất cả mọi thứ từ HCN
-//                  tao là HCN, phải có những gì của HCN     
-//KHÔNG CẦN LÀM LẠI FIELD/INTANCE VARS
 
-//3. cần phễu để đúc phiên bản mới, cần vật liệu đưa vào, gửi vào Cha giữ, đổ lên
-//  Cha, vì có mọi thứ từ Cha phiên bản gốc
-//  tôn trọng bản sắc của Con, Con HV. 
-//  code tự sinh ra sẽ căn theo phễu của Cha, 4 tham số, để đổ được 4 tham số 
-//                              phễu con phải 4, CHẾT, MẤT BẢN SẮC 
+//1. mới A extends cũ B
+//                      HCN Khuôn gốc để đổ: Class Super, Class parent, base
+//                           kế thừa từ hcn: Class Con, Class child, sub, derived  
 
-public class Square extends Rectangle {
-                                                //trade-off
-    public Square(String owner, String color, double edge) {
-        super(owner, color, edge, edge);
-    } //~~~  new Rectangle(o, c, w, l);
-    //       new hình vuông chính là new hình chữ nhật
-    //       new HV chính là new 1 phiên bản HCN 
-    //              có color, own, cạnh -> HCN dưới dạng hình vuông cũng là thế mà
+//2. Đặc điểm của con là gì??
+//              Vì HV là phiên bản HCN, nên nó sẽ có tất cả mọi thứ từ hình chữ nhật
+//                tao là HCN, tao phải có những gì HCN đang có
+
+//3. Cần phễu để đúc phiên bản mới, vật liệu đưa vào, gửi lên Cha, đổ lên 
+//     Cha, vì có mọi thứ từ Cha, phiên bản gốc
+public class Square extends Rectangle{
+
+    public Square(String owner, String color, double width, double length) {
+        super(owner, color, width, length);
+    }  //~~~~ new Rectangle(owner, color, width, length); 
+       //     new hv chính là new hcn 
+       //     new hv chính là new phiên bản hcn
+       //     cắt hv bản chất là cất hình chữ nhật. 
+       //     có color, owner, cạnh -> HCN dưới dạng hình cũng thế mà 
+//   private String owner;
+//   private String color; 
+//   private double edge; 
     
-    //VỀ LÍ ĐÃ XONG KHUÔN VUÔNG, VÌ DATA NHƯ TRUYỀN THỐNG, DÙNG LẠI HẾT CỦA CN
-    //KO CẦN VIẾT LẠI CODE, KHÔNG COPY PASTE
-    //KHÔNG CHỈNH SỬA DÂY CHUYỀN QUÁ NHIỀU, TẤT NHIÊN ĐANG DÙNG SP, SERIES
     
-    //hiện tượng hàm con trùng 100% với tên hàm cha
-    @Override //dính khái niệm gọi là tính đa hình, đa xạ, đa kế thừa
-    //đa nhân cách, đa sắc thái, 5 sắc thái, biến hình, TRANSFORMER 
-    //POLYMOPHISM
-    //HIỆN TƯỢNG CON QUA MẶT CHA, THỂ HIỆN BẢN SẮC CỦA CON, CHA NGỒI IM ĐẾ CON
-    //LÀM CHO, ĐỂ CON LÊN TIẾNG, CHA ĐỂ ĐÓ CON, VỜ NHƯ KO THẤY CHA
-    public void paint() {
-        System.out.printf("|SQUARE   |%-15s|%-10s|  - |%4.1f|%7.2f|\n", 
-                                owner, color, width, getArea());
-    }
 }
+    
+    

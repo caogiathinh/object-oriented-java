59fd78c84e19feb39ddb95bc8936fdedcf6be752
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-25 23:08:03 +0700

    Update Session06-Inheritance ParentChildV2

--- Session06-Inheritance/CraftmanV4/src/data/Shape.java
+++ Session06-Inheritance/CraftmanV4/src/data/Shape.java
@@ -14,7 +14,7 @@ public class Shape {
     protected String color; 
     protected String borderColor;
     //cạnh thì sao??
-    protected double a, b, c, radius; //còn nữa thì sao, có nên không ?? 
+    //protected double a, b, c, radius; //còn nữa thì sao, có nên không ?? 
     //khong được đưa các cạnh vào đây vì:
     //chúng sẽ kế thừa các cạnh, hình vuông sẽ có cạnh, nhiều nữa là đằng khác, bk 
     //tròn thừa kế luôn cả cạnh 
@@ -28,5 +28,79 @@ public class Shape {
     
     
     //diện tích chu vi là derived field được tính từ thằng khác không nên đưa vào đây
+
+    public Shape(String owner, String color, String borderColor) {
+        this.owner = owner;
+        this.color = color;
+        this.borderColor = borderColor;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(String borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" + "owner=" + owner + ", color=" + color + ", borderColor=" + borderColor + '}';
+    }
+    
+    //1 Khuôn vẫn theo nguyên tắc chung vẫn chế tạo theo nguyên tắc chung 
+    public void paint() {
+        
+    }
+    
+    //XÓA KHÔNG CHO VÌ CẦN KHÁI NIỆM NÀY ĐỂ DIÊN TẢ RÕ Ý NGHĨA HH
+    //ĐỂ KHÔNG ĐƯỢC VIẾT CODE, VÌ KHÔNG CỤ THỂ KÍCH THƯỚC
+    //VẪN TỒN TẠI NHƯNG KHÔNG CHẠM ĐƯỢC, VẪN TỒN TẠI VỚI NGHĨA TINH THẦN MÔ TẢ HOI, NÓI VỀ HOI NHƯNG CHƯA CỤ THẺ
+    //NÓI NHƯNG LÀM, KHÁI NIỆM THOI. 
+    //HÀM TRỪU TƯỢNG, VẤN ĐỀ TRỪU TƯỢNG, THỨ VÔ HÌNH VÀ VẪN HỮU HÌNH
+    //ĐỂ VÀO CHO ĐỦ BỘ HOÀN THIỆN 1 THỨ GÌ ĐÓ, LÃNH ĐẠO TINH THẦN, KO QUYỀN LỰC CỤ THỂ.
+    //MA 
+    
+    public double getArea() {
+        //tính thế nào ??, éo có kích thước vì dị biệt ở từng Con 
+        //return 50; //không chơi default vô nghĩa
+        //cạnh đâu để sửa lại S, công thức đâu mà tính
+        
+                           //không có kích thước sao tính được
+                           //không xóa hình được vì HH thì phải có S
+        //Hình học cần có diện tích là khái niệm, nói rằng thằng nào chiếm bề mặt, có số đo S
+        //đại diện cho 1 nhóm/series các đồ vật khác đều có S, P 
+        //để đây như là ý nghĩa tinh thần, đảm bảo để mô tả 1 thứ nào đó. 
+        //HH có S, P
+    }
+    
+    public double getPerimeter() {
+        
+    }
+    
+    //diện tích chu vi không được xem là đặc tính mà phải xem là hành vi
+    //2 hàm tính diện tích, chu vi để ở đâu??? Con hay cha, hay cả 2 ?? 
+    //cả 2, vừa là di truyền vừa là dị biệt (@Override)
+    //ĐÃ NÓI VỀ HÌNH HỌC, LÀ PHẢI NÓI VỀ CHU VI DIỆN TÍCH
+    
+    //HÌNH CỤ THỂ V TR TG HBH CHẮC CHẮN LÀ TÍNH ĐƯỢC, CŨNG LÀ ĐĐ CỦA TỪNG HÌNH  
+    //mỗi loại hình sẽ dị biệt trong tính toán nói chung là có S và P
+    //
     
 }

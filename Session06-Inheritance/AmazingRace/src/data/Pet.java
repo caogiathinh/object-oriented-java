2b2b505cb6497bd134ef3e6d2d6fb400a14fa3d1
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-27 22:45:45 +0700

    Update Session06-Inheritance AmazingRace

---
+++ Session06-Inheritance/AmazingRace/src/data/Pet.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data;
+
+/**
+ *
+ * @author gia.thinh
+ */
+public class Pet {
+    //chứa nhân tử chung của đám Con
+    protected String name; 
+    protected int yob;
+    protected double weight;
+    
+    
+}

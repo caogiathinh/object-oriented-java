0d029cf689ed081fcdefd66d077c84e81a2818da
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-28 08:57:19 +0700

    Update Session06-Inheritance AmazingRace

--- Session06-Inheritance/AmazingRace/src/data/Cat.java
+++ Session06-Inheritance/AmazingRace/src/data/Cat.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package data;
 
 /**
  *
  * @author gia.thinh
  */
-public class Cat {
+public class Cat extends Pet {
+    
+    public static final double MAX_SPEED = 50;
+    
+    private String ribbon; 
+
+    public Cat(String name, int yob, double weight, String ribbon) {
+        super(name, yob, weight);
+        this.ribbon = ribbon;
+    }
+
+    public String getRibbon() {
+        return ribbon;
+    }
+
+    public void setRibbon(String ribbon) {
+        this.ribbon = ribbon;
+    }
+
+    @Override
+    public double run() {
+        
+    }
+
+    @Override
+    public void showRecord() {
+        
+    }
+    
     
 }

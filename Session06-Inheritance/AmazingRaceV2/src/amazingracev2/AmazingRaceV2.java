b22e250a6d08211d234af206ef14cbee7e5afb24
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-29 14:31:43 +0700

    Update Session06-Inheritance AmazingRaceV2

---
+++ Session06-Inheritance/AmazingRaceV2/src/amazingracev2/AmazingRaceV2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package amazingracev2;
+
+/**
+ *
+ * @author gia.thinh
+ */
+public class AmazingRaceV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Session06-Inheritance/AmazingRaceV2/src/data/Pet.java
@@ -0,0 +1,10 @@
+
+package data;
+
+/**
+ *
+ * @author gia.thinh
+ */
+public class Pet {
+    
+}

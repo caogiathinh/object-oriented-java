eb2fc45b72aa70e27b3f5192a43601478a69d2b9
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-29 23:04:48 +0700

    Update Session06-Inheritance AmazingRaceV3

---
+++ Session06-Inheritance/AmazingRaceV3/src/amazingracev3/AmazingRaceV3.java
@@ -0,0 +1,30 @@
+package amazingracev3;
+
+import data.Cat;
+import data.Dog;
+import data.Hamster;
+import data.Pet;
+
+/**
+ *
+ * @author gia.thinh
+ */
+public class AmazingRaceV3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    public static void showRecords() {
+        Pet racer[] = new Pet[]{new Dog("CHIHUHU", 2021, 0.5),
+                                new Dog("VANG OI", 1950, 10),
+                                new Cat("TOM", 1960, 15.0),
+                                new Cat("KITTY", 1990, 5.0),
+                                new Hamster("JERRY", 1950, 0.5)
+                               };
+        
+    }
+}

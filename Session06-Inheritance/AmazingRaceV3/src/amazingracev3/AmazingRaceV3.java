5d71a43ed1da66402ef1a015940d3819dedb0162
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-10-30 20:09:28 +0700

    Update Session06-Inheritance AmazingRaceV3

--- Session06-Inheritance/AmazingRaceV3/src/amazingracev3/AmazingRaceV3.java
+++ Session06-Inheritance/AmazingRaceV3/src/amazingracev3/AmazingRaceV3.java
@@ -37,5 +37,24 @@ public class AmazingRaceV3 {
         for (Pet x : racer) {
             x.showRecord();
         }
+        
+        //TẠI THỜI ĐIỂM NÀY KHÔNG SORT DC THÀNH TÍCH
+        for (int i = 0; i < racer.length - 1; i++) {
+            for (int j = i + 1; j < racer.length; j++) {
+                if (racer[i].run() > racer[j].run()) {
+                    //đa hình. hàm Cha nhưng chạy Con tương ứng: HT, D, C,. ... 
+                    //mỗi gọi run() để so sánh thì sẽ có tốc độ khác
+                    //bài SV, hình học, S cố định cho n lần gọi 
+                    //điểm trung bình cố định cho đến khi cập nhật điểm mới
+                    //for hoải, getGpa(), getS() cố dịnh nên ta mới đảo được
+                    //bài AmazingRace này băt chạy 1 lượt ghi nhận thành tích 
+                    //ta so sánh trên thành tích chạy này
+                    
+                    Pet tmp = racer[i]; 
+                    racer[i] = racer[j];
+                    racer[j] = tmp;
+                }
+            }
+        }
     }
 }

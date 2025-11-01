f8f65a1b8747864001962dc543c8e425e0dab4c4
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-11-01 18:46:00 +0700

    Update Session06-Inheritance Interface

--- Session06-Inheritance/AmazingRace-Interface/src/data/Dog.java
+++ Session06-Inheritance/AmazingRace-Interface/src/data/Dog.java
@@ -7,7 +7,12 @@ import java.util.Random;
  *
  * @author gia.thinh
  */
-public class Dog extends Pet {
+//Dog ngoan ngoãn ban ngày, tối chủ đi ngủ nó trốn nhà đi đua
+//đi đua thì nó phải chạy vượt tốc độ bình thường như mọi đua thủ
+//mình vẫn là con cháu họ lê, phạm, .. 
+//nhưng mình vẫn tham gia CLB Đua, Thủ, Phượt, CLB gì đó, thì phải có hành động của clb đó
+
+public class Dog extends Pet implements DeathRacer {
     
     public static final double MAX_SPEED = 40;
 
@@ -24,5 +29,15 @@ public class Dog extends Pet {
     public void showRecord() {
         System.out.printf("|%-10s|%-10s|%4d|%4.1f|%4.1f|\n", 
                             "DOG", name, yob, weight, run());
+    }
+
+    @Override
+    public double runToDead() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void showHowToDead() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
--- Session06-Inheritance/AmazingRace-Interface/src/data/Motor.java
+++ Session06-Inheritance/AmazingRace-Interface/src/data/Motor.java
@@ -1,4 +1,3 @@
-
 package data;
 
 import java.util.Random;
@@ -8,7 +7,6 @@ import java.util.Random;
  * @author gia.thinh
  */
 //Chơi thêm kế thừa, Motor is a kind of Vehicle (abstract class)
-
 //MOTOR MUA VỀ CHẠY ĐI HỌC, CHUYỆN BÌNH THƯỜNG, HÀM RUN()
 //NHƯNG TRÊN ĐƯỜNG THĂNG VỚ VẪN NÀO ĐÓ NẸT PÔ, MÌNH KÉO TAY GA MẠNH HƠN.
 //BỐC ĐẦU, TỐC ĐỘ PHẢI DỮ HƠN BÌNH THƯỜNG, LÚC NÀY NGOÀI VIỆC MOTOR THÔNG THƯỜNG
@@ -19,10 +17,19 @@ import java.util.Random;
 //                                                      TỚ THAM GIA CLB, TRIỂN KHAI VIỆC DÙNG 
 //THAM GIA CLB NÀO PHẢI CÓ HÀNH ĐỘNG CỦA CLB ĐÓ
 //IMPLEMENT ALL ABSTRACT METHODS BÊN CLB, CHỌN ĐI ĐUA MÀ KHÔNG CHỊU ĐUA SAO ĐƯỢC
+//@OVERRIDE
+//CLASS CHÍNH CỦA CHÚNG TA CÓ ĐẶC ĐIỂM HVI NHƯ BÌNH THƯỜNG
+//CHÚNG TA THAM GIA CLB NÀO THÌ THÊM HOẠT ĐỘNG CỦA CLB ĐÓ,OVERRIDE HÀNH ĐỘNG ĐÓ
+//CHÚNG TA CÓ NHIỀU HÀNH ĐỘNG ỨNG VỚI TỪNG CLUB TA THAM GIA, HỢP LÍ NGOÀI ĐỜI
+//ĐI VỚI BỤT MẶC ÁO CÀ SA, ĐI VỚI MA MẶC ÁO GIẤY
+//VỀ LOGIC, TA CÓ THỂ THAM GIA N CLB, NHƯNG TA CHỈ CÓ MỘT DÒNG HỌ
+//TRONG JAVA, MỘT CON TỐI ĐA MỘT CHA, ĐƠN KẾ THỪA
+//            MỘT CON THAM GIA N CLUB, VẬY SẼ CÓ HÀNH ĐỘNG CỦA N CLB
+//TRONG C++   MỘT CON CÓ NHIỀU CHA / ĐA KẾ THỪA
 public class Motor implements DeathRacer {
-    
+
     public static final double MAX_SPEED = 180;
-    
+
     private String model;  //exciter: 2021
     private String volume; //số phân khối
     private String vin; //biển số, số khung, Vehicle Identifier Number
@@ -61,15 +68,28 @@ public class Motor implements DeathRacer {
     public String toString() {
         return "Motor{" + "model=" + model + ", volume=" + volume + ", vin=" + vin + '}';
     }
-    
+
     //hành vi của xe máy là chạy, có tốc độ được ghi nhận
-    
     public double run() {
         return new Random().nextDouble() * MAX_SPEED;
     }
-    
+
     public void showRecord() {
-        System.out.printf("|%-10s|%-15s|%-8s|%-11s|%5.1f|\n", 
-                            "MOTOR", model, volume, vin, run());
+        System.out.printf("|%-10s|%-15s|%-8s|%-11s|%5.1f|\n",
+                "MOTOR", model, volume, vin, run());
+    }
+
+    //INTERFACE - IMPLEMENTS
+    @Override
+    public double runToDead() {
+        //CHẠY TỐC ĐỘ CAO
+        return run() * 3; //run() giả sử được 150 x 3 đua đến chết còn gì? 
+
+    }
+
+    @Override
+    public void showHowToDead() {
+        System.out.printf("|%-20s|%-15s|%-8s|%-11s|%5.1f|\n",
+                "MOTOR-RACER", model, volume, vin, runToDead());
     }
 }

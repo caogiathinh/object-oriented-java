e9c8f9de658a941b0d99aea3966c8bdce34f8262
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-11-01 17:59:48 +0700

    Update Session06-Inheritance Interface

--- Session06-Inheritance/AmazingRace-Interface/src/data/Motor.java
+++ Session06-Inheritance/AmazingRace-Interface/src/data/Motor.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package data;
+
+import java.util.Random;
 
 /**
  *
  * @author gia.thinh
  */
-public class Motor {
+//Chơi thêm kế thừa, Motor is a kind of Vehicle (abstract class)
+
+//MOTOR MUA VỀ CHẠY ĐI HỌC, CHUYỆN BÌNH THƯỜNG, HÀM RUN()
+//NHƯNG TRÊN ĐƯỜNG THĂNG VỚ VẪN NÀO ĐÓ NẸT PÔ, MÌNH KÉO TAY GA MẠNH HƠN.
+//BỐC ĐẦU, TỐC ĐỘ PHẢI DỮ HƠN BÌNH THƯỜNG, LÚC NÀY NGOÀI VIỆC MOTOR THÔNG THƯỜNG
+//TA CÒN LÀ ĐUA-THỦ NỮA, PHẢI LÀ ĐUA THỦ VỚI TỐC ĐỘ CAO
+//GIA NHẬP ĐUA THOI, 
+//TỚ CÒN LÀ 1 ĐUA-THỦ, KO DÙNG TỪ KHÓA EXTENDS, XÀI TỪ LÀ 1 MEMBER
+//                                                          IMPLEMENTS
+//                                                      TỚ THAM GIA CLB, TRIỂN KHAI VIỆC DÙNG 
+//THAM GIA CLB NÀO PHẢI CÓ HÀNH ĐỘNG CỦA CLB ĐÓ
+//IMPLEMENT ALL ABSTRACT METHODS BÊN CLB, CHỌN ĐI ĐUA MÀ KHÔNG CHỊU ĐUA SAO ĐƯỢC
+public class Motor implements DeathRacer {
     
+    public static final double MAX_SPEED = 180;
+    
+    private String model;  //exciter: 2021
+    private String volume; //số phân khối
+    private String vin; //biển số, số khung, Vehicle Identifier Number
+
+    public Motor(String model, String volume, String vin) {
+        this.model = model;
+        this.volume = volume;
+        this.vin = vin;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    @Override
+    public String toString() {
+        return "Motor{" + "model=" + model + ", volume=" + volume + ", vin=" + vin + '}';
+    }
+    
+    //hành vi của xe máy là chạy, có tốc độ được ghi nhận
+    
+    public double run() {
+        return new Random().nextDouble() * MAX_SPEED;
+    }
+    
+    public void showRecord() {
+        System.out.printf("|%-10s|%-15s|%-8s|%-11s|%5.1f|\n", 
+                            "MOTOR", model, volume, vin, run());
+    }
 }

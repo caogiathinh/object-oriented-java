56fcf99c459b035fc763ae63fe73ca8a20aa1283
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-11-05 09:38:11 +0700

    Update Folder Session07-CollecttionFramework

--- Session07-CollectionFramework/StudentManagement/src/studentmanagement/StudentManagement.java
+++ Session07-CollectionFramework/StudentManagement/src/studentmanagement/StudentManagement.java
@@ -23,13 +23,22 @@ public class StudentManagement {
         //arr là 1 cái túi chứa sẵn 500 chỗ, 500 món đồ có thể bỏ vô
         //món đồ phải đi mua, new thảy vào
         //tên mảng chính là má mì quản lí nhiều đồ, tên túi luôn
+        
+        //List arr = new ArrayList(); //lộn xộn đủ loại con trỏ
+        //đúng nghĩa túi doraemon, món gì cũng được, đựng tham chiếu thoi nha
+        //nhét được NhanVien, GiangVien, Animal, ... 
+        
 
         //túi đồng nhất con trỏ chỉ lưu con trỏ Student
         List<Student> arr = new ArrayList();
         //List<Student> arr = new ArrayList<>();
         //List<Student> arr = new ArrayList<Student>();
         //arr là cái túi chứa vô hạn chỗ, vô hạn con trỏ
-        //
+        //nhét đồng nhất món đồ thì mới dễ chấm chung đa hình được
+        
+        //mở ngăn tủ, túi, nhét đồ vô
+        arr[i] = new Student(....);  //mảng
+        arr.add(new Student)
     }
 
 }

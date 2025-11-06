e0ceef19e249516f23744ac93f01528e35f24904
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-11-06 19:33:53 +0700

    Update Folder-Session06 Inheritance

--- Session06-Inheritance/AmazingRaceFinal/src/data/Motor.java
+++ Session06-Inheritance/AmazingRaceFinal/src/data/Motor.java
@@ -1,4 +1,3 @@
-
 package data;
 
 import java.util.Random;
@@ -13,15 +12,23 @@ import java.util.Random;
 //đua thủ - phải là đua thủ mới có tốc độ cao
 //gia nhập hội đua thủ
 //không dùng từ khóa Extends, xài từ là 1 member của IMPLEMENTS - TỚ THAM GIA CLB, TRIỂN KHAI VIỆC ĐUA
-
 //tham gia clb thì phải có hành động của clb đó 
 //IMPLEMENTS ALL ABSTRACT METHODS BÊN CLB, CHỌN ĐUA MÀ KHÔNG CHỊU ĐUA SAO ĐƯỢC  
-public class Motor implements DeathRacer{
-    
-    public static final double MAX_SPEED = 180; 
-    
-    private String model; 
-    private String volume; 
+//@Override
+//Class chính cứ đặc điểm hành vi như bình thường. 
+//chúng ta tham gia clb nào thì có hành động của clb đó, @Override hành động 
+//trong ta có nhiều hành động ứng với clb ta đang tham gia 
+//đi với bụt mặc áo cà sa đi với ma mặc áo giấy
+//về logic, ta có thể tham gia N câu lạc bộ, nhưng ta chỉ có 1 dòng họ  
+//trong java, 1 con tối đa 1 cha, đơn kế thừa
+//            1 con tham gia N club, VẬY SẼ GIẢI QUYẾT CÂU CHUYỆN ĐA KẾ THỪA
+//C++, 1 CON CÓ NHIỀU CHA LUÔN, ĐA KẾ THỨA!!! 
+public class Motor implements DeathRacer {
+
+    public static final double MAX_SPEED = 180;
+
+    private String model;
+    private String volume;
     private String vin; //biển số khung (vehicle Identifier Number)
 
     public Motor(String model, String volume, String vin) {
@@ -58,25 +65,27 @@ public class Motor implements DeathRacer{
     public String toString() {
         return "Motor{" + "model=" + model + ", volume=" + volume + ", vin=" + vin + '}';
     }
-    
+
     //hành vi xe máy là chạy, có tốc độ dc ghi nhận
     public double run() {
-        return new Random().nextDouble() * MAX_SPEED; 
+        return new Random().nextDouble() * MAX_SPEED;
     }
-    
+
     //quay video và gáy tốc độ 
     public void showRecord() {
-        System.out.printf("|%-10s|%-15s|%-8s|%11s|%5.1f|\n", 
-                           "MOTOR", model, volume, vin, run());
+        System.out.printf("|%-10s|%-15s|%-8s|%11s|%5.1f|\n",
+                "MOTOR", model, volume, vin, run());
     }
 
     @Override
     public double runToDead() {
-        return run() * 3; 
+        //chạy tốc độ cao, đôn nòng, xoáy nòng, ép dên, đôn dên
+        return run() * 3;
     }
 
     @Override
     public void showHowToDead() {
-        
+        System.out.printf("|%-20s|%-15s|%-8s|%11s|%5.1f|\n",
+                "MOTOR-RACER", model, volume, vin, runToDead());
     }
 }

d6b84774f303f3f3c57a84b7b0f1a8b2e11d4200
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-11-06 19:49:48 +0700

    Update Folder-Session06 Inheritance

--- Session06-Inheritance/AmazingRaceFinal/src/data/Dog.java
+++ Session06-Inheritance/AmazingRaceFinal/src/data/Dog.java
@@ -10,7 +10,13 @@ import java.util.Random;
  *
  * @author gia.thinh
  */
-public class Dog extends Pet {
+//Dog ngoan ngoãn ban ngfy tối chủ đi ngủ, nó trốn nhà đi hoang
+//đi đua thì nó phải chạy vươt tốc độ bình thường, runToDead() như mọi đua thủ. 
+//mình vẫn là con cháu nhà họ Lê Phạm, học như bình thường sinh viên
+//nhưng còn tham gia clb Đua thủ, 
+//IMPLEMENT ALL ABSTRACT METHOD, NỘI QUI CÂU LẠC BỘ
+
+public class Dog extends Pet implements DeathRacer {
     
     public static final double MAX_SPEED = 40; 
 
@@ -28,4 +34,20 @@ public class Dog extends Pet {
         System.out.printf("|%-10s|%-10s|%4d|%4.1f|%4.1f|\n", 
                            "DOG", name, yob, weight, run());
     }
+
+    
+    //PHẦN HOANG GIẢ TRONG TA, BỘC LỘ KHI ĐI ĐUA
+    //TỤI MÀY, SV, NGOAN NGOÃN Ở NHÀ GIÃN CÁCH
+    //VÀO SG, CHẮC CHẮN THÊM PHẦN BỘC LỘ KHÁC: NHẬU, GAME, CÁ ĐỘ, ĐÁNH BÀI, .. CLB TA, TỪ THIỆN, CẦU LÔNG
+    
+    @Override
+    public double runToDead() {
+        return run() * 5; 
+    }
+
+    @Override
+    public void showHowToDead() {
+        System.out.printf("|%-20s|%-10s|%4d|%4.1f|%4.1f|\n", 
+                           "DOG-RACER", name, yob, weight, runToDead());
+    }    
 }

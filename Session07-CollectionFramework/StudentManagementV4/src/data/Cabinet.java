8c6f7d44f8a1ab09ebc1b1af3ff6be600e8e1664
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-11-12 09:00:58 +0700

    Update Folder-Session07 Collections

---
+++ Session07-CollectionFramework/StudentManagementV4/src/data/Cabinet.java
@@ -0,0 +1,22 @@
+
+package data;
+
+import java.util.List;
+import data.Student;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author gia.thinh
+ */
+public class Cabinet {
+    private List<Student> arr = new ArrayList<Student>(); 
+
+    public Cabinet() {
+    }
+    
+    private Scanner sc = new Scanner(System.in); 
+    
+    
+}
--- Session07-CollectionFramework/StudentManagementV4/src/studentmanagementv4/StudentManagementV4.java
+++ Session07-CollectionFramework/StudentManagementV4/src/studentmanagementv4/StudentManagementV4.java
@@ -11,7 +11,7 @@ public class StudentManagementV4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
